d72b14f8f74dfca226edd973975435b5b2d783c6
rarry1310 <trandafir.rares@gmail.com>
2022-08-19 12:04:00 +0300

    update

--- script.js
+++ script.js
@@ -29,98 +29,109 @@ window.onload = function() {
             newTask[i].value = taskStorage[i];
         }
     }
-
 }
 
 // FUNCTIONS
 function addTask(e) {
 
-    //PREVENT THE FORM FROM SUBMITING
-    e.preventDefault();
 
-    if (input.value === "") {
-        input.setAttribute("Placeholder", "Task Name Can't Be Empty");
-        input.classList.add("placeholder-error");
-    } else {
-        input.classList.remove("placeholder-error");
+  //PREVENT THE FORM FROM SUBMITING
+  e.preventDefault();
 
-        // CREATE TASK DIV
-        let taskDiv = document.createElement('div');
-        taskDiv.classList.add('task-div');
+  if (input.value === "") {
+    alert("Please add a task!")
+  } else {
 
-        // CREATE TASK INPUT
-        let taskInput = document.createElement('input');
-        taskInput.value = input.value;
-        taskStorage.push(input.value);
+    //CREATE TASK DIV
+    let taskDiv = document.createElement('div');
+    taskDiv.classList.add('task-div');
 
-        taskInput.type = 'text';
-        taskInput.setAttribute("readonly", "readonly");
-        taskInput.classList.add('task-input', 'task-new');
-        taskDiv.appendChild(taskInput);
 
-        // CHECK BUTTON
-        createButton('check', 'check', taskDiv);
+    //CREATE TASK INPUT
+    let taskInput = document.createElement('input');
+    taskInput.value = input.value;
 
-        // EDIT BUTTON
-        createButton('edit', 'edit', taskDiv);
+    taskStorage.push(input.value)
 
-        // DELETE BUTTON
-        createButton('delete', 'eraser', taskDiv);
+    taskInput.type = 'text';
+    taskInput.setAttribute("readonly", "readonly");
+    taskInput.classList.add('task-input', 'task-new');
+    taskDiv.appendChild(taskInput);
 
-        taskList.appendChild(taskDiv);
 
-        //CLEAR INPUT VALUE
-        input.value = '';
-    }
-    updateStorage();
-}
+    //CHECK BUTTON
+    let checkButton = document.createElement('button');
+    checkButton.innerHTML = '<i class="bx bx-check"></i>';
+    checkButton.classList.add('check-btn');
+    checkButton.setAttribute("onclick", "checkTask(this);");
+    taskDiv.appendChild(checkButton);
 
-function createButton(buttonWord, iconName, taskDiv) {
-    let buttonName = document.createElement('button');
-    buttonName.innerHTML = `<i class="bx bx-${iconName}"></i>`;
-    buttonName.classList.add(`${buttonWord}-btn`);
-    buttonName.setAttribute("onclick", `${buttonWord}Task(this);`);
-    taskDiv.appendChild(buttonName);
+
+    // //EDIT BUTTON
+    let editButton = document.createElement('button');
+    editButton.innerHTML = '<i class="bx bx-edit"></i>';
+    editButton.classList.add('edit-btn');
+    editButton.setAttribute("onclick", "editTask(this);");
+    taskDiv.appendChild(editButton);
+
+
+    //DELETE BUTTON
+    let deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '<i class="bx bx-eraser"></i>';
+    deleteButton.classList.add('delete-btn');
+    deleteButton.setAttribute("onclick", "deleteTask(this);");
+    taskDiv.appendChild(deleteButton);
+
+    taskList.appendChild(taskDiv);
+
+    //CLEAR INPUT VALUE
+    input.value = '';
+  }
+  updateStorage();
 }
 
 function checkTask(taskButton) {
-    let taskInput = taskButton.parentElement.children[0];
+  let taskInput = taskButton.parentElement.children[0];
+  if (taskInput.classList == 'task-done') {
     taskInput.classList.toggle('task-done');
-    updateStorage();
+  } else {
+    taskInput.classList.toggle('task-done');
+  }
+  updateStorage();
 }
 
 function editTask(taskButton) {
-    let taskInput = taskButton.parentElement.children[0];
-    let editButton = taskButton.parentElement.children[2];
+  let taskInput = taskButton.parentElement.children[0];
+  let editButton = taskButton.parentElement.children[2];
 
-    if (taskInput.hasAttribute('readonly')) {
-        taskInput.removeAttribute('readonly');
-        taskInput.focus();
-        editButton.style.backgroundColor = '#fff';
+  if (taskInput.hasAttribute('readonly')) {
+    taskInput.removeAttribute('readonly');
+    taskInput.focus();
+    editButton.style.backgroundColor = '#fff';
 
-        localStorage.setItem("editedItem", taskStorage.indexOf(taskInput.value))
-    } else {
-        taskInput.setAttribute('readonly', 'readonly');
-        taskInput.blur();
-        editButton.style.backgroundColor = 'rgb(197, 228, 84)';
+    localStorage.setItem("editedItem", taskStorage.indexOf(taskInput.value))
+  } else {
+    taskInput.setAttribute('readonly', 'readonly');
+    taskInput.blur();
+    editButton.style.backgroundColor = 'rgb(197, 228, 84)';
 
-        taskStorage[localStorage.getItem("editedItem")] = taskInput.value;
-    }
-    updateStorage();
+    taskStorage[localStorage.getItem("editedItem")] = taskInput.value;
+  }
+  updateStorage();
 }
 
 function deleteTask(taskButton) {
-    let taskInput = taskButton.parentElement.children[0];
-    let taskDiv = taskButton.parentElement;
-    let removedItem = taskStorage.indexOf(taskInput.value)
-    if (removedItem !== -1) {
-        taskStorage.splice(removedItem, 1);
-    }
-    taskDiv.remove();
-    updateStorage();
+  let taskInput = taskButton.parentElement.children[0];
+  let taskDiv = taskButton.parentElement;
+  let removedItem = taskStorage.indexOf(taskInput.value)
+  if (removedItem !== -1) {
+    taskStorage.splice(removedItem, 1);
+  }
+  taskDiv.remove();
+  updateStorage();
 }
 
 function updateStorage() {
-    localStorage.setItem("taskStorage", taskStorage);
-    localStorage.setItem("taskList", taskList.innerHTML);
+  localStorage.setItem("taskStorage", taskStorage);
+  localStorage.setItem("taskList", taskList.innerHTML);
 }
